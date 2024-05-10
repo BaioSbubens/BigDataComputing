7d6cd6c44e5e051a27ec2c9aa6ab4a755680d15a
BaioSbubens <baietti723@gmail.com>
2024-05-10 16:24:58 +0200

    changes

---
+++ HW2/G050HW2.py
@@ -0,0 +1,199 @@
+from pyspark import SparkContext, SparkConf
+import sys
+import os
+import random as random
+import math
+import time
+
+# SPARK SETUP
+conf = SparkConf().setAppName('G050')
+sc = SparkContext(conf=conf)
+sc.setLogLevel("WARN")
+
+def distance(point1, point2):
+    diff_i = point1[0] - point2[0]
+    diff_j = point1[1] - point2[1]
+    return diff_i * diff_i + diff_j * diff_j
+"""
+def ExactOutliers(points, D, M, K):
+    start = time.time()
+    t=D*D
+    l = []
+    num_outliers = 0
+    for i in points:
+        s = 0
+        for j in points:
+            d = distance(i,j)
+            if d < t:
+                s += 1
+            if s > M:
+                break
+        if s <= M:
+            num_outliers += 1
+            l.append(i)
+    print("Number of Outliers:", num_outliers)
+    for k in sorted(l)[:K]:
+        print(f'Point: {k}')
+    finish = time.time()
+    print(f'Running time of ExactOutliers = {((finish - start)*1000):.0f} ms')
+"""
+
+def belong_cell(point,side):
+
+    i=math.floor((point[0]/side))
+    j=math.floor((point[1]/side))
+    return [(i,j)]
+
+
+def points_count_per_cell(cell):
+    pairs_dict = {}
+    for p in cell:
+        if p not in pairs_dict.keys():
+            pairs_dict[p] = 1
+        else:
+            pairs_dict[p] += 1
+    return [(key, pairs_dict[key]) for key in pairs_dict.keys()]
+
+
+
+def step_A(input,D):
+
+    side = D/(2*math.sqrt(2))
+    points_count = (input.flatMap(lambda x: belong_cell(x,side))
+                    .mapPartitions(points_count_per_cell)
+                    .groupByKey()
+                    .mapValues(lambda vals: sum(vals)))        
+    return points_count
+
+def step_B(cells,M):
+    outliers = 0
+    uncertain = 0
+    for center in cells:
+        if center[1] <= M:
+            i,j = center[0]
+            tot_3x3 = 0
+            tot_7x7 = 0
+            for cell in cells:
+                x,y = cell[0]
+                if abs(x - i) <= 1 and abs(y - j) <= 1:
+                    tot_3x3 += cell[1]
+                elif abs(x - i) <= 3 and abs(y - j) <= 3:
+                    tot_7x7 += cell[1]
+                if tot_3x3 > M:
+                    break
+            tot_7x7 += tot_3x3
+            if tot_7x7 <= M:
+                outliers += center[1]
+            elif tot_3x3 <= M:
+                uncertain += center[1]
+    return (outliers, uncertain)
+"""
+def first_k_cells(output_A,K):
+    ordered = (output_A.map(lambda x : (x[1],x[0]))
+               .sortByKey()
+               .take(K))
+    return ordered
+"""
+
+def MRApproxOutliers(points, D, M):
+    start = time.time()
+    output_A = step_A(points,D)
+    cells = output_A.collect()
+    outliers, uncertain = step_B(cells,M)
+    print(f'Number of sure outliers = {outliers}')
+    print(f'Number of uncertain outliers =  {uncertain}')
+    finish = time.time()
+    print(f'Running time of MRApproxOutliers = {((finish - start)*1000):.0f} ms')
+
+def SequentialFFT(points, K):
+    C = [points[0]]
+    while len(C) < K :
+        far_d= 0
+        far_p = None
+        for el in points:
+            if el not in C:
+                d = min(distance(el,center) for center in C)
+                if d > far_d:
+                    far_d = d
+                    far_p = el
+        C.append(far_p)
+    return C
+
+def radius(inputPoints, C):
+    centroids = C
+    r = 0
+    for el in inputPoints:
+        dist = min(distance(el, center) for center in centroids)
+        if dist > r:
+            r = dist
+    return [r]
+
+
+def MRFFT(InputPoints,K):
+    #Round1
+    start_R1 = time.time()
+    corset = InputPoints.mapPartitions(lambda x:SequentialFFT(list(x),K)).collect()
+    finish_R1 = time.time() 
+    #Round2
+    start_R2 = time.time()
+    final_centroids = SequentialFFT(corset,K)
+    finish_R2 = time.time()
+    #Round3
+    start_R3 = time.time()
+    C = sc.broadcast(final_centroids)
+    rad = (InputPoints.mapPartitions(lambda x: radius(x, C.value))
+              .reduce(lambda x,y: max(x,y)))
+    finish_R3 = time.time()
+
+    print(f'Running time of MRFFT Round 1 = {((finish_R1 - start_R1)  *1000):.0f} ms')
+    print(f'Running time of MRFFT Round 2 = {((finish_R2 - start_R2)  *1000):.0f} ms')
+    print(f'Running time of MRFFT Round 3 = {((finish_R3 - start_R3)  *1000):.0f} ms')
+    print(f'Final Centroids = {final_centroids}')
+    print(f'Radius = {rad}')
+    return rad
+
+
+
+
+def main():
+
+    # CHECKING NUMBER OF CMD LINE PARAMTERS
+    assert len(sys.argv) == 5, "Usage: python G050.py <file_name> <M> <K> <L> " 
+
+    # SETTING GLOBAL VARIABLES
+    
+    M = int(sys.argv[2])
+    K = int(sys.argv[3])
+
+    # INPUT READING
+
+    # 1. Read number of partitions
+    L = int(sys.argv[4])
+    
+    # 2. Read input file and subdivide it into L random partitions
+    data_path = sys.argv[1]
+    #assert os.path.isfile(data_path), "File or folder not found"
+    rawData = sc.textFile(data_path).repartition(numPartitions=L).map(lambda line: line.split(","))
+    inputPoints= rawData.map(lambda x: (float(x[0]),float(x[1]))).cache()
+    
+    print(f'{sys.argv[1]} M={M} K={K} L={L}')
+    n = inputPoints.count()
+    print(f'Number of points = {n}')
+    D = MRFFT(inputPoints,K)
+    MRApproxOutliers(inputPoints, D, M)
+    
+
+if __name__ == "__main__":
+	main()
+
+'''
+def SequentialFFT(points,K):
+    C= [(points[0])]
+    while len(C) < K:  
+        dis = []
+        for point in points:
+            dis.append(min([distance(point, center) for center in C]))
+        pos = np.argmax(dis)
+        C.append(points[pos])
+    return C
+    '''
