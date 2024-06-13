4416f1fb86cbd4e7a725a06c094d3ecfed339a49
BaioSbubens <baietti723@gmail.com>
2024-06-13 08:36:04 +0200

    final

--- HW3/G050HW3.py
+++ HW3/G050HW3.py
@@ -8,49 +8,34 @@ import math
 import numpy as np
 from collections import Counter
 
-# Global parameters
-  # To be set via command line
+def process_batch(batch):
+    global streamLength, histogram, reservoir, sticky
+    batch_items = batch.collect()
 
-# Sticky Sampling parameters
-S = {}
-current_bucket = 0
-bucket_width = 0
-
-
-# Reservoir Sampling parameters
-reservoir = []
-reservoir_size = 0
-
-# Operations to perform after receiving an RDD 'batch' at time 'time'
-def process_batch(time, batch):
-    global streamLength, histogram, current_bucket, bucket_width, S, epsilon, phi, delta, reservoir, reservoir_size
-    batch_items = batch.map(lambda s: int(s)).collect()
-    
-
-    
     for item in batch_items:
-        if streamLength >= THRESHOLD:
+        if streamLength >= n:
             stopping_condition.set()
             return
         streamLength +=1
-        #True frequent items
+
+        # True frequent items
         histogram[item] = histogram.get(item, 0) + 1
 
-        #Reservoir sampling
+        # Reservoir sampling
         if len(reservoir) < reservoir_size:
-            reservoir.append(item)
+            reservoir.append(int(item))
         else:
             rand_index = random.randint(0, streamLength - 1)
             if rand_index < reservoir_size:
-                reservoir[rand_index] = item
+                reservoir[rand_index] = int(item)
         
         # Sticky Sampling
         r = (math.log(1/(delta * phi)))/epsilon
-        if item in S:
-            S[item] += 1
+        if item in sticky:
+            sticky[item] += 1
         else:
-            if random.random() <= r/THRESHOLD:
-                S[item] = 1
+            if random.random() <= r/n:
+                sticky[item] = 1
     
 
 
@@ -64,10 +49,6 @@ if __name__ == '__main__':
     delta = float(sys.argv[4])
     portExp = int(sys.argv[5])
     
-    THRESHOLD = n
-    reservoir_size = math.ceil(1 / phi)
-    bucket_width = math.ceil(1 / epsilon)
-    
     # Spark configuration
     conf = SparkConf().setMaster("local[*]").setAppName("G050HW3")
     sc = SparkContext(conf=conf)
@@ -76,29 +57,36 @@ if __name__ == '__main__':
 
     # Stopping condition
     stopping_condition = threading.Event()
-    
+
     # Data structures to maintain the state of the stream
-    streamLength = 0  # Stream length (an array to be passed by reference)
-    histogram = {}  # Hash Table for the distinct elements
-    
+    streamLength = 0  # Stream length
+
+    # True frequent items parameters
+    histogram = {} 
+
+    # Reservoir Sampling parameters
+    reservoir = []
+    reservoir_size = math.ceil(1 / phi)
+
+    # Sticky Sampling parameters
+    sticky = {}
+
     # Process the stream
     stream = ssc.socketTextStream("algo.dei.unipd.it", portExp, StorageLevel.MEMORY_AND_DISK)
-    stream.foreachRDD(lambda time, batch: process_batch(time, batch))
+    stream.foreachRDD(lambda batch: process_batch(batch))
     
     # Start the streaming engine
     ssc.start()
     stopping_condition.wait()
-    ssc.stop(False, True)
+    ssc.stop(False, False)
 
     # Compute and print final statistics
-    true_frequent_items = sorted([item for item in histogram if histogram[item] / streamLength >= phi])
-    epsilon_frequent_items = sorted(S.keys())
-
+    
     print("INPUT PROPERTIES")
     print("n =", streamLength, "phi =", phi, "epsilon =", epsilon, "delta =", delta, "port =", portExp)
-    
     print("EXACT ALGORTIHM")
     print("Number of items in the data structure =", len(histogram))
+    true_frequent_items = sorted([int(item) for item in histogram if histogram[item] / streamLength >= phi])
     print("Number of true frequent items =", len(true_frequent_items))
     print("True frequent items:")
     for item in true_frequent_items:
@@ -114,10 +102,10 @@ if __name__ == '__main__':
         print(item, sign)
     
     print("STICKY SAMPLING")
-    print("Number of items in the Hash Table =", len(S))
-    S_frequent = {key : val for key,val in S.items() if val >= (phi - epsilon)*THRESHOLD}
-    print("Number of estimated frequent items =", len(S_frequent))   
+    print("Number of items in the Hash Table =", len(sticky))
+    s_frequent = [int(item) for item in sticky if sticky[item] >= (phi - epsilon)*n]
+    print("Number of estimated frequent items =", len(s_frequent))   
     print("Estimated frequent items:")
-    for item in sorted(S_frequent):
+    for item in sorted(s_frequent):
         sign = "+" if item in true_frequent_items else '-'
         print(item, sign)
